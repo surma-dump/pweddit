da7720336c736c25dc9125ec091f66e30d1b5748
Surma <surma@surma.link>
2016-06-02 16:46:58 +0100

    Fix SystemJS in ServiceWorker

--- app/sw.es5.js
+++ app/sw.es5.js
@@ -6,6 +6,10 @@ self.addEventListener('install', function(event) {
   event.waitUntil(self.skipWaiting());
 });
 
+System.config({
+  scriptLoad: true
+});
+
 System.import('/modules/ServiceWorkerController.js')
   .then(function(ServiceWorkerController) {
     new ServiceWorkerController.default();
