a8aadb0c3f7a3d04c9cf2835342ad8503937fd19
Surma <surma@surma.link>
2018-02-05 09:12:09 -0800

    Switch to watch mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
+const webpack = require('webpack');
 const copy = require('copy-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 
 module.exports = {
   entry: {
@@ -23,6 +26,14 @@ module.exports = {
   plugins: [
     new copy([
       {from: './src/index.html'}
-    ])
-  ]
+    ]),
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: 'common',
+    //   filenake: 'common.js',
+    //   // children: true,
+    //   // deepChildren: true,
+    // }),
+    // new BundleAnalyzerPlugin()
+  ],
+  watch: true,
 };
